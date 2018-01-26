073435766f944f18335a6c21cfe70450ceee22ff
zalivanvit <v.zalivan@dunice.net>
2018-01-26 11:18:33 +0300

    Touchstart

--- script/script.js
+++ script/script.js
@@ -1,14 +1,34 @@
 $(document).ready(function() {
 
+    $("body").on("touchstart", function(){
+        if ( $(window).width() < 769 ) {
+            if ( ( window.scrollY < $('#business-information').offset().top ) ){
+                $('.navigation').hide();
+                $('#step_1').show()
+            } else if (( window.scrollY >= $('#business-information').offset().top ) && ( window.scrollY < $('#additional-information').offset().top )) {
+                $('.navigation').hide();
+                $('#step_2').show()
+            } else if (( window.scrollY >= $('#additional-information').offset().top ) && ( window.scrollY < $('#services').offset().top )) {
+                $('.navigation').hide();
+                $('#step_3').show()
+            } else if (( window.scrollY >= $('#services').offset().top )) {
+                $('.navigation').hide();
+                $('#step_4').show()
+            }
+        }
+    });
+
     $(".content").css("display", "none");
 
     $(".content").fadeIn(500);
 
-    $("a.buttons__slide").click(function(){
+    $(".buttons__slide").click(function(){
         $('html, body').animate({
             scrollTop: $($(this).attr('href')).offset().top
         }, 1000);
 
             return false;
         });
+
+
 });
